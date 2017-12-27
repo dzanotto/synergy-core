8add9b4661e4ca6bbbf4640615c32191881c8450
Dario Zanotto <d.zanotto@gmail.com>
2017-12-27 18:56:41 +0100

    Added build dependencies instruction to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# To install build dependecies execute the following command:
+# sudo apt install cmake make g++ xorg-dev libqt4-dev libcurl4-openssl-dev libavahi-compat-libdnssd-dev libssl-dev libx11-dev
+
 ./hm.sh clean
 ./hm.sh conf -g1
 ./hm.sh build
